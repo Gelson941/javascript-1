8f62f0d8a5b491d0cf5edc56fe1285b703358910
Gelson94 <gelson.t94@gmail.com>
2023-07-13 09:32:55 -0300

    exercicios js 1

---
+++ operacoes.js
@@ -0,0 +1,7 @@
+const operacoes = require('./operacoes.js');
+
+// Exemplos de uso das funções
+console.log(operacoes.soma(2, 3)); // Saída: 5
+console.log(operacoes.multiplicacao(4, 5)); // Saída: 20
+console.log(operacoes.divisao(10, 2)); // Saída: 5
+// ... e assim por diante
